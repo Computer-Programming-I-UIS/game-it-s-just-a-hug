acc0172f8bdc2c0ab18c55e945d7507e72a496e6
Konat Sebastian <juansebastianguerrero2001@gmail.com>
2023-03-17 09:40:18 -0500

    Comentarios agregados a la clase jugador

--- python_version/player.py
+++ python_version/player.py
@@ -58,7 +58,7 @@ class Player():
         
         
         self.y_speed+=self.y_acceleration
-        
+        # Tener en cuenta teclas presionadas y direcciones bloqueadas
         if self.keys[pygame.K_d] and not self.direcctions['Right']:
             self.x_speed=general_speed
             
@@ -67,16 +67,17 @@ class Player():
         else:
             self.x_speed=0
         
-        if self.keys[pygame.K_w] and not self.direcctions['Up'] and self.direcctions['Down']:
+        if self.keys[pygame.K_w] and not self.direcctions['Up'] and self.direcctions['Down']: 
+            #Si esta libre arriba y esta sobre el suelo
             self.y_speed=-general_speed*2
         elif not self.direcctions['Down']:
             if self.y_speed > 10:
                 self.y_speed = 5
-            pass
+            
             #self.y_speed=-general_speed
         else: 
             self.y_speed=0
-         #   self.y_speed=0
+         
         
         
         self.Player.left+=self.x_speed      
@@ -123,7 +124,9 @@ class Player():
         
         for bloque in bloques:
             direcctions={'Up': False ,'Down':False ,'Left':False , 'Right': False} #limpiar colisiones
+            # La direccion en la que este true, sera una direccion que esta bloqueada
             collisions_tolerance = 10
+            
             
             if self.Player.colliderect(bloque.block):
                 if abs(bloque.block.top-self.Player.bottom) < collisions_tolerance: #toca por arriba al bloque
@@ -142,7 +145,7 @@ class Player():
                     direcctions['Right']= True
                     self.Player.right = bloque.block.left
                 
-                # Si se encuentra con una esquina (hay dos Trues)
+                # Si se encuentra con una esquina (hay dos direcciones bloqueadas por el mismo bloque)
                 if not (direcctions['Down'] ^ direcctions['Up'] ^ direcctions['Left'] ^ direcctions['Right']):
                     print ("Encontre una esquina")
                     print("En y: ",self.y_speed)
@@ -156,19 +159,23 @@ class Player():
                         direcctions['Left']= False
                         direcctions['Right']= False    
                     elif abs(self.y_speed) > 0 and abs(self.x_speed) > 0:
+                        # Si se va moviendo en las dos direcciones, entonces quitar todos los bloqueos y dejar que otro bloque decida
                         direcctions['Up']= False
                         direcctions['Down']= False
                         direcctions['Left']= False
                         direcctions['Right']= False 
                         
                 else: 
-                    print("No encontre")
+                    print("No encontre esquina")
+            
+            # Si hay un true debido a este bloque, se agrega a los bloqueos
             self.direcctions['Up']=self.direcctions['Up'] or direcctions['Up']
             self.direcctions['Down']=self.direcctions['Down'] or direcctions['Down']
             self.direcctions['Left']=self.direcctions['Left'] or direcctions['Left']
             self.direcctions['Right']=self.direcctions['Right'] or direcctions['Right']
                         
         
+        # Bloqueos con los bordes de la pantalla
         if self.Player.right >= screen_size_x:
             self.direcctions['Right']= True
             #self.x_speed *= -1 #borrar
@@ -185,6 +192,20 @@ class Player():
             #self.y_speed *= 0 #borrar
             
     def closest_object(self, list_objets):
+        """
+        Esta funcion calcula la lista de objetos que estan cerca
+
+        Parameters
+        ----------
+        list_objets : list
+            lista de todos los objetos en pantalla.
+
+        Returns
+        -------
+        close_objects : list
+            lista de los objetos cercanos.
+
+        """
         close_objects=[]
         for obj in list_objets:
             
